733c686402872f22837c73546d601533b792775b
ix1osbek <bekerkinov2004@gmail.com>
2025-03-29 05:43:30 +0500

    Product schema added

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,8 @@
 import { Router } from "express"
+import Product from "../models/Product.js";
 const router = Router()
 router.get("/", (req, res) => {
-    const token = req.cookies.token; // Cookie'dan tokenni olish
+    const token = req.cookies.token
     const isLoggedIn = token ? true : false;
     res.render("index", {
         title: "Erkinov SHOP",
@@ -22,6 +23,15 @@ router.get("/add", (req, res) => {
         title: "add",
         isAdd: true
     })
+})
+
+router.post("/add-products", async (req, res) => {
+    const { title, description, image, price } = req.body
+    const products = await Product.create(req.body)
+    console.log(products);
+    
+    res.redirect("/")
+
 })
 
 export default router
